1089f2e2ea61d6bd2d0ef431ceb039b0ed17a529
Daniel Carvalho <daniel.carvalho@runtime-revolution.com>
2025-02-05 11:18:39 +0000

    Add CI

---
+++ lib/sh/heroku_deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Usage: bin/heroku_deploy
+# Script to run migrations on Heroku after deployment. Based on the following GIST:
+# https://gist.github.com/stevenharman/98576bf49b050b9e59fb26626b7cceff
+# Should be called on Procfile for the release phase.
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NO_COLOR='\033[0m'
+
+set -euo pipefail
+
+schema_version=$(bin/rails db:version | { grep "^Current version: [0-9]\\+$" || true; } | tr -s ' ' | cut -d ' ' -f3)
+
+if [ -z "$schema_version" ]; then
+  printf "💀${RED}   [Release Phase]: Database schema version could not be determined. Does the database exist?${NO_COLOR}\n"
+  exit 1
+fi
+
+if [ "$schema_version" -eq "0" ]; then
+  printf "\n⏳${YELLOW}   [Release Phase]: Loading the database schema.${NO_COLOR}\n"
+  bin/rails db:schema:load
+else
+  printf "\n⏳${YELLOW}   [Release Phase]: Running database migrations.${NO_COLOR}\n"
+  bin/rails db:migrate
+fi
+
+printf "\n🎉${GREEN}   [Release Phase]: Database is up to date.${NO_COLOR}\n"
